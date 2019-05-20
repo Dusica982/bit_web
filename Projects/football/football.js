b8519516599c87eec13946822fccaa0135144d47
dusica982 <dusica.antic@gmail.com>
2019-05-20 22:37:36 +0200

    sredjeno do pocetka 5. nedelje

---
+++ Projects/football/football.js
@@ -0,0 +1,22 @@
+var field = document.querySelector('div');
+var player = document.querySelector('#image-player');
+var stopmove = document.querySelector('#stop-moving');
+
+function movingPlayer(event) {
+
+    player.style.top = event.clientY + "px";
+    player.style.left = event.clientX + "px";
+
+}
+
+field.addEventListener("click", movingPlayer);
+
+
+function stopMoving() {
+    player.style.top = "0px";
+    player.style.left = "0px";
+    field.removeEventListener("click", movingPlayer);
+
+}
+
+stopmove.addEventListener('click', stopMoving);
